ec30fb3afee52bafeb34d7aeeb02062eaff95506
Brian Whitton <brian.whitton@wsj.com>
2021-01-05 23:02:37 -0500

    some cleanup

--- README.md
+++ README.md
@@ -14,11 +14,11 @@ Open `.next/static/chunks/pages/index-<hash>.js`
 
 Look for `"should not be here"`. Shouldn't be there!
 
-Compare the output if you change the commented line in `pages/index.js` to return `top.msg`:
+Compare the output if you change the commented line in `pages/index.js` to return `top.foo`:
 ```js
 // pages/index.js
 export default function Home() {
-  //return nested.nested.msg;
-  return top.msg;
+  //return nested.foo.bar;
+  return top.foo;
 }
 ```
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 const nested = {
-  nested: {
-    msg: 'NESTED: should be here',
-    qux: {
-      zuk: 'should not be here'
+  foo: {
+    bar: 'NESTED: should be here',
+    baz: {
+      qux: 'NESTED: should not be here'
     }
   },
 };
 
 const top = {
-  msg: 'TOPLINE: should be here',
-  qux: {
-    zuk: 'should not be here',
+  foo: 'TOP: should be here',
+  bar: {
+    baz: 'TOP: should not be here',
   }
 };
 
 export default function Home() {
-  return nested.nested.msg;
-  // return top.msg;
+  return nested.foo.bar
+  // return top.foo;
 }
